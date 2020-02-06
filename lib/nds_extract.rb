54f4e12834b3f4b9fd5dbc26f085c084ff23ddcf
bryan lesht <bryan.lesht@colorado.edu>
2020-02-06 19:00:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,7 +5,8 @@ require 'directors_database'
 # using director_data as input
 def gross_for_director(director_data)
   file = File.open("dir.rb", "w")
-  file << pp directors_database
+  pp_directors_database = pp directors_database
+  file << pp_directors_database
   file.close
 
 end
