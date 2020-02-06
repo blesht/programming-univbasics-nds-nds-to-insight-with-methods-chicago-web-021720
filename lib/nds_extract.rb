43bf7eec1c7242df09e524d12f55a71394a72c67
bryan lesht <bryan.lesht@colorado.edu>
2020-02-06 21:34:54 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,7 +21,10 @@ end
 
 
 def directors_totals(nds)
-  result = {}
-  nil
+  director_index = 0 
+  while director_index < nds.length do 
+  gross_for_director(nds[director_index])
+  director_index += 1 
+  end 
 end
 
