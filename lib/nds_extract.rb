3351940aa0ac49634c85e9fba3330a4aa689577d
bryan lesht <bryan.lesht@colorado.edu>
2020-02-06 19:06:34 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,7 +4,9 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  
+  file = File.open("data.rb", "w")
+  file << director_data
+  file.close
 
 end
 
